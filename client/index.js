22ae8020645be6f26b3c6763fa5c2ec6a2718b3a
Hayley Witherell <hayleywitherell@gmail.com>
2021-07-26 14:35:14 -0600

    Answer 2nd prompts

--- client/index.js
+++ client/index.js
@@ -18,6 +18,48 @@ fetch("http://localhost:3001/api/v1/animals", {
 // 4. On page load, fire off #1 from above and populate the section (.js-collection) with those items
 // 5. Add an event listener, so that when you click button.js-add-item, it will make a fetch request to POST that item to the server, AND update the page with the newly updated collection
 
+const collection = document.querySelector('.js-collection')
+const addUser = document.querySelector('.js-add-item')
+let fetchedData;
+addUser.addEventListener('click', addUsers);
+window.addEventListener('load', function() {
+  getData('sport-teams')
+})
+function addUsers(data) {
+  postData(4, 'Fara', 'Jason Hughes', 'Golf');
+  data.map(user => {
+    collection.innerHTML += `<h3> Id: ${user.id} </h3>
+    <p>UserName: ${user.name}</p>
+    <p>Coach: ${user['head_coach']}</p>
+    <p>Sport: ${user.sport}</p> `
+  })
+}
+const getData = (path) => {
+  fetch(`http://localhost:3001/api/v1/${path}`)
+    .then(response => response.json())
+    .then(data => {
+      fetchedData = data
+      addUsers(data)
+    })
+}
+const postData = (id, name, coach, sport) => {
+  let newObj = {
+    id: id,
+    name: name,
+    head_coach: coach,
+    sport: sport,
+  }
+  fetch('http://localhost:3001/api/v1/sport-teams', {
+      method: 'POST',
+      body: JSON.stringify(newObj),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
 // EXTRA CREDIT:
 // Create a form and event listener so that the user can add an item of their choosing
 // Handle the use case where a 422 is given back from the server (this will happen if not all params are sent with the POST)
